f19b80f5dbcad1f7914cae8c17f2ff9c7fd61bee
LukeMinnich <squareman2357@gmail.com>
2025-11-22 13:26:50 -0800

    basic http server fetch

--- src/main.zig
+++ src/main.zig
@@ -7,21 +7,15 @@ pub fn main() !void {
     try comprende.bufferedPrint();
 }
 
+const http = std.http;
+
 test "simple test" {
     const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    var client = std.http.Client{ .allocator = gpa };
+    defer client.deinit();
+    const req = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = "https://www.google.com?q=zig" },
+    });
+    std.debug.print("status={d}\n", .{req.status});
 }
